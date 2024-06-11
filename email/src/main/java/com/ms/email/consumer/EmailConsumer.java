3716795a9190b6637570109fc33265360956e878
GiselleBarbosa <g88barbosa@gmail.com>
2024-06-10 22:42:51 -0300

    https://github.com/GiselleBarbosa/java-user-microservice

---
+++ email/src/main/java/com/ms/email/consumer/EmailConsumer.java
@@ -0,0 +1,15 @@
+package com.ms.email.consumer;
+
+import com.ms.email.dtos.EmailRecordDto;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailConsumer {
+
+    @RabbitListener(queues = "${broker.queue.email.name}")
+    public void listenEmailQueue(@Payload EmailRecordDto emailRecordDto) {
+        System.out.println(emailRecordDto.emailTo());
+    }
+}
